513d807293c0678b731056eee749770f9c7047d8
unknown <eteuboudurel@gmail.com>
2021-11-26 18:59:25 +0100

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,44 +9,77 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   constructor(){}
   input:string ="";
-  result:string ="";
-  op ="";
-  d="";
-  c="";
   sub="";
+  temp:string ="";
+  resultat ="0."
+  c="";
   i=0;
   err=0;
-  recent:boolean =false;
+  j=0;
+  no=0;
+  vide=0;
   ngOnInit(){
 
   }
 
   delete(){
-    if (this.input !="" && !(this.input=="SYNTAX ERROR") && this.sub=="") {
-      this.input=this.input.substr(0, this.input.length-1);
+    if(this.temp !=""&& this.input[this.input.length]!="0" && this.input.substr(this.input.length-2 , this.input.length)!="++" && this.input.substr(this.input.length-2 , this.input.length)!="--" ){
+      this.temp=this.temp.substr(0, this.temp.length-1);
     }
+    if (this.input !="" && !(this.input=="SYNTAX ERROR") && !(this.input=="MATH ERROR") && this.sub=="") {
+      this.input=this.input.substr(0, this.input.length-1);
+      this.j-=this.j;
+    }
+   
   }
   clear(){
-    this.result = "";
     this.input ='';
+    this.resultat="0."
+    this.temp="";
+    this.j=0;
   }
 
   ecrire(valu:string){
+    this.c=valu;
+    if(this.input[this.input.length-1]=="/" && valu=="0"){
+      this.no=1;
+    }
     if(valu=="0"){
       if(this.input.length==0){
-        valu="";
+        this.c="";
       }
     else if ((this.input[this.input.length-1]=="+")||(this.input[this.input.length-1]=="*")||(this.input[this.input.length-1]=="-")||(this.input[this.input.length-1]=="/")){
-      valu="";
+      this.c="";
     }
     }
     if((this.input[this.input.length-1]=="+")||(this.input[this.input.length-1]=="*")||(this.input[this.input.length-1]=="-")||(this.input[this.input.length-1]=="/")){ 
-      if((valu=="+")||(valu=="*")||(valu=="-")||(valu=="/")){ 
+      if((valu=="*")||(valu=="/")){ 
         this.err=1;
+      }
+    }
+    if((this.input[this.input.length-1]=="*")||(this.input[this.input.length-1]=="/")){ 
+      if((valu=="*")|| (valu=="/")){ 
+        this.err=1;
+      }
+    }
+    if((this.input[this.input.length-1]=="+")){ 
+      if((valu=="+")){ 
+        this.c="";
+      }
+    }
+    if((this.input[this.input.length-1]=="-")){ 
+      if((valu=="-")){ 
+        this.c="";
       }
     }
     if(this.sub[this.sub.length-1]=="=" && !(valu=="+")&& !(valu=="*") && !(valu=="-") && !(valu=="/")){
       this.input="";
+      this.j=0;
+    }
+    if(this.sub[this.sub.length-1]=="=" ){
+      if((valu=="+")||(valu=="*")||(valu=="-") || (valu=="/"))
+      this.input="Ans";
+      this.j=0;
     }
     if(valu=="("){
       this.i+=1;
@@ -54,21 +87,52 @@ export class AppComponent implements OnInit {
     if(valu==")"){
       this.i-=1;
     }
+   /* if(this.j>20){
+      valu="";
+      this.c="";
+    }*/
+    if(!(valu=="")){
+      this.j+=1;
+    }
+    
+    this.temp=this.temp+this.c;
     this.input=this.input+valu;
     this.sub="";
+    this.c="";
   }
 
   egal(){
-    
-    if(this.i!=0 || this.err==1||(this.input[0]=="*")||(this.input[0]=="/")||(this.input[this.input.length-1]=="*")||(this.input[this.input.length-1]=="/")||(this.input[this.input.length-1]=="+")||(this.input[this.input.length-1]=="-")){
-      this.input="SYNTAX ERROR"
-    }else if(eval(this.input).length>14){
-      this.input=this.input.substr(0,15);
+    if(this.input=="" || this.input=="."){
+      this.vide=1;
     }
-    else{this.input=eval(this.input)}
-
+    if(this.i!=0 || this.err==1||(this.input[0]=="*")||(this.input[0]=="/")||(this.input[this.input.length-1]=="*")||(this.input[this.input.length-1]=="/")||(this.input[this.input.length-1]=="+")||(this.input[this.input.length-1]=="-")||(this.temp==".") ||(this.no==1)){
+      if(this.temp=="."){
+        this.resultat="0.";
+        this.temp="";
+      }else if(this.no==1){
+        this.input="MATH ERROR";
+        this.resultat="";
+      
+      }else{
+      this.input="SYNTAX ERROR"
+      this.resultat="";
+      this.temp="";}
+    }
+    else if(this.vide!=1){
+      this.resultat=eval(this.temp);
+      this.temp=this.resultat;
+      
+    }
+    
+   /* if(this.resultat=""){
+      this.resultat="0."
+    }*/
     this.sub=this.sub+"=";
     this.i=0;
     this.err=0;
+    this.no=0;
+    this.vide=0;
+    
+    
   }
 }
